aeaeef9f0b444aebb11f05cea6772a72f557a3c0
Fowzy S <fwzsas@gmail.com>
2021-08-26 10:21:44 -0500

    New

---
+++ Assignments/A04/A04.cpp
@@ -0,0 +1,21 @@
+/**
+* Fowzy Alsaud
+* PT 4883 - Dr. Griffin
+* August 26th, 2021
+*/
+#include <iostream>
+
+#define endl "\n"
+
+using namespace std;
+
+int main() {
+    int B = 0, A = 0;
+    
+    while (cin >> A >> B) {
+        
+        // your stuff here
+
+    }
+    return 0;
+}
